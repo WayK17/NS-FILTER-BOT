cb57b679dcd5bdde97693efa1c40504a927e8909
WayK17 <109327502+WayK17@users.noreply.github.com>
2025-03-01 15:21:29 -0500

    Actualizar info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Información del Bot
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))  # COMPLETAR CON TU API_ID
+API_ID = int(environ.get('API_ID', '15353803'))  # COMPLETAR CON TU API_ID
 API_HASH = environ.get('API_HASH', '')   # COMPLETAR CON TU API_HASH
 BOT_TOKEN = environ.get('BOT_TOKEN', "") # COMPLETAR CON TU BOT_TOKEN
 
