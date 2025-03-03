02602180c759150a4498036f3ce80ab465ad3774
WayK17 <109327502+WayK17@users.noreply.github.com>
2025-03-03 02:39:36 -0500

    Actualizar pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1823,7 +1823,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
             )
     else:
         btn.append(
-            [InlineKeyboardButton(text="NESS Cloud",callback_data="pages")]
+            [InlineKeyboardButton(text="ᑎᗴՏՏ ᑕᒪOᑌᗪ",callback_data="pages")]
         )
     imdb = await get_poster(search, file=(files[0])['file_name']) if settings["imdb"] else None
     cur_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
