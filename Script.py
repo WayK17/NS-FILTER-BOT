bc08c5bd230ac417bb23c96267b602c3e1f62270
WayK17 <109327502+WayK17@users.noreply.github.com>
2025-03-05 09:01:44 -0500

    Actualizar Script.py

--- Script.py
+++ Script.py
@@ -220,10 +220,9 @@ Mensaje <b>: {}</b>"""
 ⚙️ Tamaño: {file_size}</b>"""
 
     IMDB_TEMPLATE_TXT = """
-<b>Resultado de Búsqueda 🔍: {qurey}
-
+<b>🔍 | Resultado de Búsqueda : {qurey}
 🎬 | IMDB Detalles:
-━━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━━━━━━━━━
 🏷 | <b>Título</b>: <a href={url}>{title}</a>
 🎭 | Géneros: {genres}
 📆 | Año: <a href={url}/releaseinfo>{year}</a>
@@ -232,10 +231,10 @@ Mensaje <b>: {}</b>"""
 📀 | Duración: {runtime} minutos
 📆 | Fecha de lanzamiento: {release_date}
 🎛 | País: <code>{countries}</code>
-━━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━━━━━━━━━
 ⏰ Resultado mostrado en: {remaining_seconds} <i>segundos</i> 🔥
 ━━━━━━━━━━━━━━━
-📨 | Solicitado Por: {message.from_user.mention}</b>"""
+📨 | Solicitado Por: {message.from_user.mention} 🐒</b>"""
 
     ALL_FILTERS = """
 <b>Hola {}, estos son mis tres tipos de filtros.</b>"""
